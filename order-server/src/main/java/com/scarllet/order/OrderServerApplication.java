02389a509cd416281f0b09bec2a0149da342b244
Yuanzr <81491531@163.com>
2018-07-04 23:51:02 +0800

    添加注释

--- order-server/src/main/java/com/scarllet/order/OrderServerApplication.java
+++ order-server/src/main/java/com/scarllet/order/OrderServerApplication.java
@@ -4,14 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableEurekaClient
+@EnableEurekaClient //作为服务客户端
+@EnableFeignClients //开启feign客户端
 @SpringBootApplication
 public class OrderServerApplication {
 
-
+	/**
+	 * 创建restTemplate
+	 * @return
+	 */
 	@Bean
 	@LoadBalanced
 	RestTemplate restTemplate() {
